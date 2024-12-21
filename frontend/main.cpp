f67d87c60c00eaaeff6ed9bbffda82116c4bf24d
creeper82 <66107554+creeper82@users.noreply.github.com>
2024-12-21 10:40:57 +0100

    Cleanup main.cpp

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -5,14 +5,12 @@
 #include <QtWidgets>
 
 #include "mainwindow.h"
-#include "bitmap.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    Bitmap* bitmap = new Bitmap(2,3);
-    MainWindow window(bitmap);
-    window.setMinimumSize(1000, 400);
+    
+    MainWindow window;
     
     window.show();
     return app.exec();
